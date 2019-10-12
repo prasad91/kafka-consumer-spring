a63def1b1822eb26f404fb0d5ea2637d55956612
Prasad <vavilala.prasad@gmail.com>
2019-10-12 12:35:57 -0400

    kafka-consumer spring

--- kafka-consumer-example/src/main/java/com/example/prasad/kafka/consumer/App.java
+++ kafka-consumer-example/src/main/java/com/example/prasad/kafka/consumer/App.java
@@ -1,13 +1,17 @@
 package com.example.prasad.kafka.consumer;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World! - Consumer");
+		SpringApplication.run(App.class, args);
+	}
+
 }
